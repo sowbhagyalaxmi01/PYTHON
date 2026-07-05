4480ad02484f53f2cc255895282ee69078168b6e
sowbhagyalaxmi01 <sowbhagyalaxmi1805@gmail.com>
2026-07-05 14:29:45 +0530

    added introduction

---
+++ day1/introduction.py
@@ -0,0 +1,112 @@
+# what is python?
+#  python is high level programming language ,interpreted,programming language .It is created by Gudio Van Rossum in 1991.
+#  Python is known for its simple syntax, readability, and powerful libraries.
+    
+# where python is used?
+   # Web Development (Backend)
+   # Software Development
+   # Artificial Intelligence (AI)
+   # Machine Learning (ML)
+   # Data Science Data Analysis
+   # Cloud Computing
+   # Automation & Scripting
+   # Cybersecurity
+   # Game Development
+
+#  
+# what can python do?
+
+# Python can:
+
+#    Create web applications.
+#    Automate repetitive tasks.(it is performing task again and again with same steps or process)
+#    Connect to databases.
+#    Read, write, and modify files.
+#    Analyze large amounts of data.
+#    Perform mathematical calculations.
+#    Build AI and Machine Learning models.
+#     Develop desktop and command-line applications.
+#    Create APIs and web services.
+
+
+# Why Python?
+#   Python is popular because:
+#   1. Easy to Learn
+#   Its syntax is similar to the English language.
+#   Example:
+#   print("Hello World")
+
+#   2. Platform Independent
+#   Python programs run on:
+#   Windows
+#    Linux
+#   macOS
+#   Raspberry Pi
+#   without changing the code.
+
+#   3. Less Code
+#     Compared to many programming languages, Python requires fewer lines of code.
+
+#   4. Interpreted Language
+#   Python executes code line by line, so there is no compilation step before running the program.
+
+# Advantages:
+#   Easier debugging
+#   Faster testing
+#   Rapid development
+#   Supports Multiple Programming Paradigms
+
+# Python supports:
+#   Procedural Programming
+#   Object-Oriented Programming (OOP)
+#   Functional Programming
+
+# Good to Know
+#   The latest major version is Python 3.
+#   Python code can be written in:
+#   VS Code
+#   PyCharm
+#   Thonny
+#   IDLE
+#   Eclipse
+#   NetBeans
+
+
+# Python Syntax Compared to Other Languages
+#   1. Easy to Read
+#   Python is designed to be readable and resembles plain English.(It looks similar to normal English sentences)
+
+# Python syntax:
+#     Rules for writing the python code
+# key points:
+#     simple and readable.
+#     no semicolons(;)needed
+#     no curly braces
+#     code is written line by line
+
+# what can i become if i learn python.
+#     Python Developer.
+#     web developer.
+#     ML engineer.
+#     AI developer.
+#     game developer.
+#     Data analyst.
+#     Automation engineer.
+
+
+# comments:
+# they helps to explain what code does and makes it easier to understand.
+# comments are lines in program ignored by python interpreter.
+# Comments are notes written inside the code for humans to read. Python ignores them during execution.
+#     1.single line comment:#
+#     2.muti line comment:""" """
+
+# why comments are important
+#     to understand the code better in further
+#     Improves code reliability
+#     Useful when working with teams
+
+# output :
+# print() function to display text or output values.
+# Text in Python must be inside quotes. You can use either " double quotes or ' single quotes.
+# If you want to print multiple words on the same line, you can use the end parameter.
