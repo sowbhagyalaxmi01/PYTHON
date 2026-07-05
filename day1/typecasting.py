b0cc7c1090a5ed9ad221d3f5b95077bea30f0546
sowbhagyalaxmi01 <sowbhagyalaxmi1805@gmail.com>
2026-07-05 16:55:11 +0530

    added type conversion

---
+++ day1/typecasting.py
@@ -0,0 +1,18 @@
+# Type Conversion
+#   implicit type conversion.
+#   Type conversion is the automatic conversion of one data type into another by Python.
+#   a = 10      # int
+#   b = 2.5     # float
+#   c = a + b
+#   print(c)
+#   print(type(c))
+#   output:12.5
+#   <class 'float'>
+
+# Type Casting:
+#    explicit type conversion
+#   Type casting is the manual conversion of one data type into another by the programmer using built-in functions.
+#   Uses functions like int(), float(), str(),bool(),dict(),list(),tuple()
+#   x = "50"
+#   print(int(x) + 10)
+#   output:60
