b37a6817465788a0a2baadc80df561117be8e676
sowbhagyalaxmi01 <sowbhagyalaxmi1805@gmail.com>
2026-07-06 17:36:23 +0530

    added modifying strings

---
+++ day1/modifystring.py
@@ -0,0 +1,18 @@
+#upper case: the upper() methods returns string in upper case.
+#lower case:the lower() methods returns the string in lower case.
+#remove whitespace:strip()
+#split string into sub string pritn(a.split(","))
+#join() print("".join(txt.split())
+# f-strings to combine string and numbers by adding curly brackets{} as placeholders for variables and other operators.
+#    eg:price=34
+#    txt=f"the price is {price} dollars"
+#    print(txt)
+#escape character (/)used to Print special characters (quotes, backslashes)
+#   Format text (new lines and tabs)
+#   Represent Unicode and special control characters
+#   Make strings easier to read and display correctly
+#   \n → New line
+#   \t → Tab
+#   \\ → Backslash
+#   \' → Single quote
+#   \" → Double quote
