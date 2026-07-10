6eeed487a621b6e2f1f784fa5587d717c1ce5b38
sowbhagyalaxmi01 <sowbhagyalaxmi1805@gmail.com>
2026-07-10 23:59:32 +0530

    added 10 problems dict

--- day5/intermediatepro.py
+++ day5/intermediatepro.py
@@ -1,9 +1,9 @@
 #Find the sum of all values in a dictionary.
 d = {"a": 10, "b": 20, "c": 30}
-sum=0
-for "value" in d.values():#print(sum(d.values()))
-    sum+=1
-print(sum)
+total=0
+for value in d.values():#print(sum(d.values()))
+    total+=value
+print(total)
 
 #Find the maximum value.
 d = {"a": 10, "b": 20, "c": 30}
@@ -14,3 +14,70 @@ for value in values:#print(max(d.values()))
     if value > maximum:
         maximum = value
 print(maximum)
+
+#Find the minimum value.
+d = {"a": 10, "b": 20, "c": 30}
+minimum=None
+for value in d.values():
+    if minimum is None or value<minimum:
+        minimum=value
+print(minimum)        
+
+
+#Count how many values are even.
+d = {"a": 10, "b": 20, "c": 30,"d":40}
+even=0
+odd=0
+for value in d.values():
+    if value%2==0:
+        even+=1
+    else:
+        odd+=1
+print("even:",even)  
+print("odd:",odd)         
+
+#Multiply all values together.
+d = {"a": 10, "b": 20, "c": 30,"d":40}
+product=1
+for value in d.values():
+    product*=value
+print(product)
+
+   
+#Create a new dictionary with all values doubled.
+d = {"a":10, "b":20, "c":30, "d":40}
+
+new = {}
+
+for key, value in d.items():
+    new[key] = value * 2
+
+print(new)
+
+
+#Swap keys and values.
+d = {"a":10, "b":20, "c":30, "d":40}
+
+new = {}
+
+for key, value in d.items():
+    new[value] = key
+
+print(new)
+
+
+#Merge two dictionaries.
+d={"a":1,"b":2}
+x={"e":4,"g":9}
+d.update(x)
+print(x)
+
+#Clear all items from a dictionary.
+d = {"a": 10, "b": 20, "c": 30,"d":40}
+d.clear()
+print(d)
+
+
+
+
+
