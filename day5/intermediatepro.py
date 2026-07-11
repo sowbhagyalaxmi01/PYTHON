a226b87c96acf7c6563bd422061f928d35e55e4f
sowbhagyalaxmi01 <sowbhagyalaxmi1805@gmail.com>
2026-07-11 13:26:28 +0530

    added updated problems

--- day5/intermediatepro.py
+++ day5/intermediatepro.py
@@ -77,6 +77,91 @@ d = {"a": 10, "b": 20, "c": 30,"d":40}
 d.clear()
 print(d)
 
+#Count the frequency of each character in a string using a dictionary.
+d="sowbhagya"
+freq = {}
+for ch in d:
+    if ch in freq:
+        freq[ch] += 1
+    else:
+        freq[ch] = 1
+print(freq)
+
+#Count the frequency of each word in a sentence.
+d = "this is a vscode page"
+freq = {}
+for word in d.split():
+    if word in freq:
+        freq[word] += 1
+    else:
+        freq[word] = 1
+print(freq)    
+
+#Find the key with the highest value.
+d = {"a":10,"b":20,"c":30,"d":40}
+maximum = None
+max_key = None
+for key, value in d.items():
+    if maximum is None or value > maximum:
+        maximum = value
+        max_key = key
+print(max_key)
+
+
+#Find the key with the lowest value.
+d = {"a":10,"b":20,"c":30,"d":40}
+minimum = None
+min_key = None
+for key, value in d.items():
+    if minimum is None or value < minimum:
+        minimum = value
+        min_key = key
+print(min_key)
+
+#Sort the dictionary by keys.
+d = {"c":30,"a":10,"d":40,"b":20}
+for key in sorted(d):
+    print(key, d[key]) 
+
+#Sort the dictionary by values.
+d = {"a":10,"b":20,"c":30,"d":40}
+
+for key, value in sorted(d.items(), key=lambda x: x[1]):
+    print(key, value)
+
+
+#Remove duplicate values.
+d = {"a":10,"b":20,"g":10,"c":30}
+new = {}
+for key, value in d.items():
+    if value not in new.values():
+        new[key] = value
+print(new)
+
+
+#Find common keys between two dictionaries.
+d = {"a":1,"b":2}
+x = {"b":4,"g":9}
+for key in d:
+    if key in x:
+        print(key)
+
+
+#Check if two dictionaries are equal.
+d = {"a":1,"b":2}
+x = {"b":4,"g":9}
+print(d == x)
+
+
+#Convert two lists into a dictionary.
+keys = ["a","b","c"]
+values = [10,20,30]
+d = {}
+for i in range(len(keys)):
+    d[keys[i]] = values[i]
+
+print(d)
+
 
 
 
