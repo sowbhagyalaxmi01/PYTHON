18de5659113cab0a97cea73195ea685bada1df22
sowbhagyalaxmi01 <sowbhagyalaxmi1805@gmail.com>
2026-07-27 22:28:05 +0530

    added basic code of funtion

---
+++ day20/funtions.py
@@ -0,0 +1,38 @@
+#funtion is block of code it can be reused.
+
+#funtion with return 
+#return sends a value back to the caller and ends the function. The returned value can be stored in a variable and used later.
+def fun_name():
+    return "hi"
+print(fun_name())
+
+
+def greet():
+    return "Hi"
+
+result = greet()      # Value is returned 
+print(result)#hi
+
+#funtion with only print no return
+#print() only displays output on the screen. It does not return a useful value, so the function returns None by default if there is no return.
+def name():
+    print( "hi")
+name()
+
+
+def greet():
+    print("Hi")#hi 
+
+result = greet()
+print(result)#None
+
+
+      
+
+
+
+
+
+
+      
+            #None
