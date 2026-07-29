9344f67d42f423d3ca18087d37ecbe156955ac89
sowbhagyalaxmi01 <sowbhagyalaxmi1805@gmail.com>
2026-07-29 22:46:58 +0530

    updated map with example

--- day20/mapuserinput.py
+++ day20/mapuserinput.py
@@ -3,3 +3,22 @@ def fun_add(a,b):
     return a+b
 m,n=map(int,input().split(","))
 print(fun_add(m,n))
+
+
+
+#Without unpacking operations returns in tuple form
+def operations(a,b):
+    return a+b ,a-b,a*b,a/b
+m,n=map(int,input().split())
+print(operations(m,n))
+
+#packing operations
+def operations(a, b):
+    return a + b, a - b, a * b, a / b
+a, b = map(int, input().split())
+addition, subtraction, multiplication, division = operations(a,b)
+
+print(addition)
+print(subtraction)
+print(multiplication)
+print(division)
