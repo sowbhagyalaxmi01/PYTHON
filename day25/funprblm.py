30dbf53842ab3259f66a5b17bd493833891cfe75
sowbhagyalaxmi01 <sowbhagyalaxmi1805@gmail.com>
2026-08-01 22:07:28 +0530

    added problems

---
+++ day25/funprblm.py
@@ -0,0 +1,36 @@
+#Count vowels
+def count_vowels(s):
+    count=0
+    for ch in s.lower():
+        if ch in "aeiou":
+            count+=1
+    return count        
+print(count_vowels("Saturday"))   
+
+#Reverse a string
+def reverse(s):
+   return s[::-1]
+s=input("enter a string:")
+print(reverse(s))
+
+def reverse(s):
+    rev=""
+    for ch in s:
+        rev=ch+rev
+    return rev
+s=input("enter a string:")    
+print(reverse(s))
+
+#Check string palindrome
+def palindrome(s):
+    if s==s[::-1]:
+        return "palindrome"
+    else:
+        return "not palindrome"
+print (palindrome("sdfg"))    
+
+  
+#
+
+
+    
