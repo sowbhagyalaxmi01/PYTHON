2889861e56be7043fbbfabe4fae8558bb875da5c
sowbhagyalaxmi01 <sowbhagyalaxmi1805@gmail.com>
2026-08-03 00:03:28 +0530

    added update prblm

--- day25/funprblm.py
+++ day25/funprblm.py
@@ -30,7 +30,16 @@ def palindrome(s):
 
 
   
-#
+##. Sum of digits
+def digit_sum(n):
+    total = 0
+    while n > 0:
+        total += n % 10
+        n //= 10
+    return total
+print(digit_sum(1234))
+
+
 
 
     
