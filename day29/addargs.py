5933ccbae909fef3f04429554b5cd0fd84b6770e
sowbhagyalaxmi01 <sowbhagyalaxmi1805@gmail.com>
2026-08-08 23:08:10 +0530

    added add args

--- day29/addargs.py
+++ day29/addargs.py
@@ -17,23 +17,19 @@ print(total(10,23,45))
 def show(a, b, *c):
     first = a + b
     remaining = sum(c)
-
     return first, remaining
-
 print(show(10, 20, 30, 40, 50))
 
 
 #Sum only odd numbers
 def odd_sum(*args):
     total = 0
-
     for x in args:
         if x % 2 != 0:
             total += x
-
     return total
-
 print(odd_sum(10, 15, 20, 25, 30))
+
 
 #Sum values greater than 10
 def greater_sum(*args):
