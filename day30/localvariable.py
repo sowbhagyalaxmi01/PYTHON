5d8d1d77e55436f41a8e4cafd03e9e8a9272db97
sowbhagyalaxmi01 <sowbhagyalaxmi1805@gmail.com>
2026-08-09 22:44:47 +0530

    added local scope problems

--- day30/localvariable.py
+++ day30/localvariable.py
@@ -9,7 +9,6 @@ display()
 #Can we access a local variable outside
 def test():
     x = 10
-
 test()
 print(x)#NameError:name 'x' is not defined
 
@@ -18,7 +17,6 @@ print(x)#NameError:name 'x' is not defined
 def test():
     x = 50
     print(x)
-
 print("Before")
 test()
 print("After")#The local variable x is created when test() executes.
@@ -36,35 +34,31 @@ def second():
 first()
 second()#Each function has its own local x.
 
+
 #Local variable and if
 def test():
     if True:
         x = 10
-
     print(x)
-
 test()#Python does not create a separate scope for an if block. x is still local to test().
 
 
 #Local variable before assignment
 x = 100
-
 def test():
     print(x)
     x = 20
-
 test()
 #UnboundLocalError:x=20 inside the function and therefore treats x as local throughout that function.so print(x)  tries to use the local x before it has a value.
 
+
+
 #local and global
 x = 100          # Global
-
 def display():
     x = 50       # Local
     print(x)     # L → finds 50
-
 display()
-
 print(x)         # Global x → 100
 
 
@@ -77,13 +71,13 @@ def test():
     print(x + y)#x and y are local variables
 test()
 
+
+
 #if conditional block
 def test():
     a = 10
-
     if True:
         b = 20       # Still inside function's local scope
-
     print(b)       
 
 
@@ -91,7 +85,5 @@ def test():
 def test():
     for i in range(3):
         x = i
-
     print(x)
-
 test()
