882d8626a788efc62493884af1c8315ea40e5939
sowbhagyalaxmi01 <sowbhagyalaxmi1805@gmail.com>
2026-08-14 22:50:41 +0530

    added lambda funtion

---
+++ day36/lambdafun.py
@@ -0,0 +1,12 @@
+#A lambda function is a small, one-line function.
+#lambda arguments: expression
+#lambda → tells Python "I'm creating a small function"
+# a, b   → inputs
+# :       → separates inputs from result
+# a + b   → result
+
+
+#Two numbers
+multiply = lambda a, b: a * b
+
+print(multiply(4, 5))
