7c8f82bb7b7d1f7d4e829ca0dd0f8fd88d3a5538
sowbhagyalaxmi01 <sowbhagyalaxmi1805@gmail.com>
2026-08-15 11:47:14 +0530

    updated lambada funtions

--- day36/lambdafun.py
+++ day36/lambdafun.py
@@ -10,3 +10,29 @@
 multiply = lambda a, b: a * b
 
 print(multiply(4, 5))
+
+
+#Create a lambda function that returns the square of a numbe
+square = lambda x: x * x
+
+print(square(5))
+
+#Create a lambda function that returns True if a number is even.
+even = lambda x: x % 2 == 0
+
+print(even(10))
+
+#Create a lambda function that returns the bigger of two numbers.
+big = lambda a, b: a if a > b else b
+
+print(big(10, 20))
+
+#Sort by second value
+students = [("A", 80), ("B", 60), ("C", 90)]
+
+result = sorted(students, key=lambda x: x[1])
+
+print(result)
+
+
+#
