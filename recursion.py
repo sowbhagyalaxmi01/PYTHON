c3c32f6e53731cc9a3ac08df3bdc7b7248bd5b09
sowbhagyalaxmi01 <sowbhagyalaxmi1805@gmail.com>
2026-08-16 22:33:33 +0530

    added recursion

---
+++ recursion.py
@@ -0,0 +1,29 @@
+#Recursion is a programming technique where a function calls itself to solve a smaller version of the same problem.
+
+# Two important parts of recursion
+
+# ① Base Case
+#  Tells the function when to stop.
+# ② Recursive Case
+#  The function calls itself with a smaller/simpler problem.
+
+
+#Why recursion is used
+# Recursion is useful when a problem can be broken into smaller problems of the same type.
+# Common examples:
+# Trees
+# Graphs
+# Backtracking
+# Divide and conquer
+# Factorial
+# Fibonacci
+
+#Basic structure
+# def function(n):
+
+#     if base_condition:
+#         return
+
+#     # work
+
+#     function(smaller_problem)
