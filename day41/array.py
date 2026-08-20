9745233532a92bf99be63ffba0f8d6b56ed0f219
sowbhagyalaxmi01 <sowbhagyalaxmi1805@gmail.com>
2026-08-20 21:50:28 +0530

    added array

---
+++ day41/array.py
@@ -0,0 +1,13 @@
+# Python does not have built-in support for Arrays, but Python Lists can be used instead.
+#What is an Array?
+#An array can hold many values under a single name, and you can access the values by referring to an index number.
+
+
+#Access the Elements of an Array
+#an array element by referring to the index number.
+cars = ["Ford", "Volvo", "BMW"]
+cars[0] = "Toyota"
+print(cars)
+
+#The Length of an Array
+#Use the len() method to return the length of an array (the number of elements in an array).
