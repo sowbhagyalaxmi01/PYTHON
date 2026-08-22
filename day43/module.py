c69c981cf5ef64d524dc05d3d4e41ba45ab6a09d
sowbhagyalaxmi01 <sowbhagyalaxmi1805@gmail.com>
2026-08-22 21:31:05 +0530

    added module

---
+++ day43/module.py
@@ -0,0 +1,32 @@
+#In Python, a module is a file containing Python code (functions, classes, and variables) that can be reused in other Python programs. 
+# Modules help organize code and avoid rewriting the same functionality.
+
+#Types of Modules
+
+# 1.Built-in Modules
+# These come with Python and do not require installation.
+# Examples:
+# math – Mathematical functions
+# random – Generate random numbers
+# os – Operating system interactions
+# sys – System-specific parameters
+# datetime – Date and time operation
+
+#2.User-defined Modules
+# Modules created by the programmer.
+
+#Third-party Modules
+# Developed by others and installed using pip.
+# Examples:
+# numpy – Numerical computing
+# pandas – Data analysis
+# matplotlib – Data visualization
+# requests – HTTP requests
+
+
+#Advantages of Modules
+# Promotes code reusability.
+# Makes programs organized and modular.
+# Simplifies maintenance and debugging.
+# Avoids duplication of code.
+# Allows sharing of code across multiple projects.
