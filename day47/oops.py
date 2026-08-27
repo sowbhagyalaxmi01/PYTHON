472f104209d52a5cfb1b053cb6e8a500277ac582
sowbhagyalaxmi01 <sowbhagyalaxmi1805@gmail.com>
2026-08-27 16:26:32 +0530

    added oops

---
+++ day47/oops.py
@@ -0,0 +1,16 @@
+#oops is a programming approach where we organize a program using objects and classes.
+# python is object orientd programming language to structure code using class and objects for better organization and resuability.
+#oops prvides clear structure to programs
+# code resuability
+# easy to maintain
+# data security
+# reduces code duplication
+
+#class are blue print of creating a objects.
+class MyClass:#MyClass is class 
+    x=5
+
+#object is an instannce of class
+p1=MyClass()#p1 is object and myclass creates an obj from class
+print(p1.x)
+
