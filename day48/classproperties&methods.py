f7651fdf6aafbeaa9532a822315f0681120b5df0
sowbhagyalaxmi01 <sowbhagyalaxmi1805@gmail.com>
2026-08-29 22:18:50 +0530

    updated

--- day48/classproperties&methods.py
+++ day48/classproperties&methods.py
@@ -8,9 +8,11 @@ class Student:
         print(self.name, "is studying")
 
 
+
 # Objects / Instances
 s1 = Student("Rahul", 20)
 s2 = Student("Priya", 21)
+
 
 # Access properties
 print(s1.name)
@@ -18,6 +20,8 @@ print(s1.age)
 
 print(s2.name)
 print(s2.age)
+
+
 
 # Call method
 s1.study()
