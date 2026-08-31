2b29cf3879da736197ee56b95c0eed55e79ce769
sowbhagyalaxmi01 <sowbhagyalaxmi1805@gmail.com>
2026-08-31 23:04:02 +0530

    addded oops

---
+++ day50/encapsulation.py
@@ -0,0 +1,8 @@
+#encapsulation is wrapping of data and funtion into single unit(object)
+#data +funtion called as capsule
+# It helps hide internal data from direct outside access.
+# Data is usually kept private and accessed through public methods like getters/setters.
+# Improves data security and control.
+# Makes code easier to maintain and modify.
+# Prevents unwanted or invalid changes to data.
+# Example: private int balance; can be accessed/changed through methods like getBalance() and setBalance().g
