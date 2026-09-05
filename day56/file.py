2bf9e6529106965a5698cc8e7dcd003d1e3c9b84
sowbhagyalaxmi01 <sowbhagyalaxmi1805@gmail.com>
2026-09-05 22:06:21 +0530

    added files

---
+++ day56/file.py
@@ -0,0 +1,28 @@
+#File handling in Python means creating, opening, reading, writing, and managing files such as .txt, .csv, .json, etc.
+
+#2. Main file operations
+# You should learn these in this order:
+# Open a file
+# Read a file
+# Write to a file
+# Append to a file
+# Close a file
+# with open() — recommended way
+# File modes: r, w, a, x
+# Reading line by line
+# Working with CSV files
+# Working with JSON files
+
+# Why do we use files?
+# To store data permanently.
+# To retrieve data later.
+# To handle large amounts of data.
+# To share data between programs.
+
+# File Modes
+# Mode	Meaning
+# r	Read
+# w	Write; overwrites existing content
+# a	Append data at the end
+# x	Create a new file
+# # r+	Read and write
