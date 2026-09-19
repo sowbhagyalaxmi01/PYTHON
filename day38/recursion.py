e95d871b5a7ed87e6f05471c3d8281081284749b
sowbhagyalaxmi01 <sowbhagyalaxmi1805@gmail.com>
2026-09-19 22:55:20 +0530

    updated recursion

--- day38/recursion.py
+++ day38/recursion.py
@@ -41,3 +41,62 @@ def count_digits(n):
     return 1 + count_digits(n // 10)
 
 print(count_digits(12345))
+
+
+#Print numbers from 1 to N
+def numbers(n):
+    if n==0:
+        return 
+    numbers (n-1)
+    print(n)
+print(numbers(5))
+
+
+#Print numbers from N to 1
+def numbers(n):
+    if n==0:
+        return 
+    print(n)
+    numbers(n-1)
+numbers(5)   
+
+
+#Find the sum of numbers from 1 to N using recursion.
+def sum(n):
+    if n==0:
+        return 0
+    return n+sum(n-1)
+
+print(sum(5))    
+
+
+#Find the factorial of N using recursion.
+def fact(n):
+    if n==1:
+        return 1
+    return n*fact(n-1)
+print(fact(5))
+
+
+# Sum of digits
+def sum(n):
+    if n==0:
+        return 0
+    return n%10+sum(n//10)
+print(sum(1234))
+
+#Find the largest digit
+def largest(n):
+    if n == 0:
+        return 0
+
+    return max(n % 10, largest(n // 10))
+print(largest(123445))
+
+
+#Find the smallest digit
+def smallest(n):
+    if n <10:
+        return n
+    return min(n % 10, smallest(n // 10))
+print(smallest(12345))
