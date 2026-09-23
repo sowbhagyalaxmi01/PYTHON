9d7c902426d7b8ac153bb3bf3997627ca5168947
sowbhagyalaxmi01 <sowbhagyalaxmi1805@gmail.com>
2026-09-23 23:44:39 +0530

    added utility

---
+++ python interview coding/day61/pythonutility.py
@@ -0,0 +1,27 @@
+# What is a Utility in Python?
+# A utility means a small tool or function that helps us perform a common task easily.
+# Think of it like tools in a toolbox:
+
+# Calculator → performs calculations
+# Clock → works with time
+# File manager → works with files
+# Random number generator → creates random values
+
+# In Python, utility modules provide ready-made functions so we don't have to write everything from scratch.
+
+
+
+# Why do we use utilities?
+# The main purpose is:
+# Save time + reuse existing functionality + make programs easier
+# For example:
+# Utility	Used for
+# math	Mathematical calculations
+# datetime	Date and time
+# random	Random values
+# os	Operating-system operations
+# pathlib	Files and folders
+# json	JSON data
+# csv	CSV files
+# re	Pattern matching
+# collections	Specialized data structures
